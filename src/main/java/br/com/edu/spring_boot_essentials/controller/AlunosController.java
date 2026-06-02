4c69cfd89988bcc3b7f659e2eb0d2a2994b217c4
Eduardo Dragone <dragone.dev@gmail.com>
2026-06-02 17:48:08 -0300

    feat: implement delete operation for alunos with transactional support

--- src/main/java/br/com/edu/spring_boot_essentials/controller/AlunosController.java
+++ src/main/java/br/com/edu/spring_boot_essentials/controller/AlunosController.java
@@ -43,6 +43,12 @@ public class AlunosController {
         return alunosService.getAlunoAvaliacao(alunoId);
     }
 
+    @DeleteMapping("/{alunoId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerAluno(@PathVariable Integer alunoId) throws NotFoundException{
+        alunosService.deletarAluno(alunoId);
+    }
+
 
 
 }
--- src/main/java/br/com/edu/spring_boot_essentials/service/AlunosService.java
+++ src/main/java/br/com/edu/spring_boot_essentials/service/AlunosService.java
@@ -2,12 +2,16 @@ package br.com.edu.spring_boot_essentials.service;
 
 import br.com.edu.spring_boot_essentials.database.model.AlunosEntity;
 import br.com.edu.spring_boot_essentials.database.model.AvaliacoesFisicasEntity;
+import br.com.edu.spring_boot_essentials.database.model.TreinosEntity;
 import br.com.edu.spring_boot_essentials.database.repository.IAlunosRepository;
+import br.com.edu.spring_boot_essentials.database.repository.IAvaliacoesFisicasRepository;
+import br.com.edu.spring_boot_essentials.database.repository.ITreinosRepository;
 import br.com.edu.spring_boot_essentials.dto.AlunoDto;
 import br.com.edu.spring_boot_essentials.exception.BadRequestException;
 import br.com.edu.spring_boot_essentials.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +19,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AlunosService {
 
+    private final IAvaliacoesFisicasRepository avaliacoesFisicasRepository;
+    private final ITreinosRepository treinosRepository;
     private final IAlunosRepository alunosRepository;
 
     public void criarAluno(AlunoDto alunoDto) throws BadRequestException{
@@ -36,7 +42,7 @@ public class AlunosService {
         return alunosRepository.findAll();
     }
     public AvaliacoesFisicasEntity getAlunoAvaliacao(Integer alunoId) throws NotFoundException {
-        AlunosEntity aluno =alunosRepository.findByIdFetch(alunoId)
+        AlunosEntity aluno = alunosRepository.findById(alunoId)
                 .orElseThrow(() -> new NotFoundException("Aluno não encontrado"));
 
         AvaliacoesFisicasEntity avaliacao = aluno.getAvaliacaoFisica();
@@ -46,5 +52,27 @@ public class AlunosService {
         }
 
         return avaliacao;
+    }
+
+    @Transactional
+    public void deletarAluno(Integer alunoId) throws NotFoundException{
+
+        AlunosEntity aluno = alunosRepository.findById(alunoId)
+                .orElseThrow(() -> new NotFoundException("Aluno não encontrado"));
+
+        //1. deletar treinos relacionados ao aluno
+        List<Integer> treinosAlunoIds = aluno.getTreinos().stream()
+                .map(TreinosEntity::getId)
+                .toList();
+
+        treinosRepository.deleteAllById(treinosAlunoIds);
+
+        //2. deletar o aluno
+
+        alunosRepository.deleteById(alunoId);
+
+        //3. deletar avaliação física
+
+        avaliacoesFisicasRepository.deleteById(aluno.getAvaliacaoFisica().getId());
     }
 }
